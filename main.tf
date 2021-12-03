5a5ca841f055ba234726436f3d88045b2410ad05
shyjithd <shyjithd@gmail.com>
2021-12-04 00:09:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aci" {
   url            = "https://192.168.1.100"
   username       = "admin"
   password       = "cisco123"
-  allow_insecure = true
+  
 }
 
 resource "aci_tenant" "tennant_1" {
