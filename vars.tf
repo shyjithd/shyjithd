eeec7550cdcad11d53289b188fa4264de3ccbacb
shyjithd <shyjithd@gmail.com>
2021-12-08 18:06:08 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "apic_host" {
+  type = string
+  description = "Hostname or IP address of your APIC controller" 
+}
+
+variable "apic_user" {
+  type = string
+  description = "Username for apic"
+}
+
+variable "apic_pass" {
+  type = string
+  description = "The password for apic"
+}
