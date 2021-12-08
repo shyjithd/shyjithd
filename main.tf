615fc69c5402ac1b16b280c072884e16b91f4b93
shyjithd <shyjithd@gmail.com>
2021-12-08 12:13:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aci = {
-      source = "CiscoDevNet/aci"
-      version = "1.0.1"
-    }
-  }
-}
-
-provider "aci" {
-  url            = "https://192.168.1.100"
-  username       = "admin"
-  password       = "cisco123"
-}
-
 resource "aci_tenant" "test" {
   name        = "ignw-tenant-1"
   description = "IGNW Tennant 1"
