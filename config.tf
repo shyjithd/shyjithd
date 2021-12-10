4a2678a76bd84b26a3b0452a3a5adf17e8b583e6
shyjithd <shyjithd@gmail.com>
2021-12-10 23:38:56 +0530

    Delete config.tf

--- config.tf
+++
@@ -1,6 +0,0 @@
-resource "aci_tenant" "terraform_tenant" {
-
-  name        = "terraform_tenant"
-  name_alias  = "tenant_for_terraform"
-  description = "This tenant is created by terraform ACI provider"
-}
